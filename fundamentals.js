45dfa8e243fb749895d8937b6cc8645dac06245e
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-15 15:16:13 +0600

    Fundamentals JS FIle Done

--- fundamentals.js
+++ fundamentals.js
@@ -1,52 +1,64 @@
-// 1. How to declare a variable using let and const
-const fatherName = 'Arnold';
-let season = 'rainy';
-season = 'winter';
+// 01. How to declare a variable using let and const
+const fatherName = "Arnold";
+let season = "Rainy";
+season = "Winter";
 
-// 2. conditions
-// 6 basic conditions: >,<, ===, !==, <=, >= 
-// multiple conditions: &&, ||
+console.log(season);
 
-if (fatherName === 'arnold' || season === 'rainy') {
 
+/*
+* 02. ----- Conditions -----
+* 6 Basic Conditions: >, <, ===, !==, <=, >= 
+* Multiple Conditions: &&, ||
+*/
+
+if (fatherName === "arnold" || season === "Rainy") {
+    console.log("Yes");
 }
-else if (fatherName === 'Arnold') {
-
+else if (fatherName === "Arnold") {
+    console.log("No");
 }
 else {
-
+    console.log("Very Good");
 }
 
-// 3. array declare
-// index, 
-// length, push, 
+
+/*
+* 03. ----- Array Declare -----
+* index, 
+* length, push, pop
+*/
 const numbers = [89, 35, 98, 12];
 numbers[0] = 56;
 
-// 4. for loop 
+console.log(numbers);
+
+
+// 04. for Loop 
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
-    confirm.log(number);
+    console.log(number);
 }
+
 
 // 5. function 
 function multiply(num1, num2) {
     const result = num1 * num2;
     return result;
 }
+const output = multiply(10, 8);
+console.log(output);
 
-const output = multiply(35, 78);
 
 // 6. Object
 // 3 ways to access property by name 
 const student = {
-    name: 'Salib Khan',
-    age: 32,
-    movies: ['king khan', 'Dhakar Mastan']
+    name: "SRK",
+    age: 60,
+    movies: ["Ra-One", "Paathan", "Jawan"]
 }
-
-const myVariable = 'age';
+const myVariable = "age";
 
 console.log(student.age); // direct by property
-console.log(student['age']);// access via property name string 
+console.log(student["age"]);// access via property name string 
 console.log(student[myVariable]); // access via property name in a variable
