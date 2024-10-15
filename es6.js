d2c07c6e5b296f3f6a6e0ac7461a821cedb17704
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-15 15:35:52 +0600

    ES6 JS FIle Done

--- es6.js
+++ es6.js
@@ -1,37 +1,43 @@
 const numbers = [89, 35, 98, 12];
-const student = {
-    name: 'Salib Khan',
-    age: 32,
-    movies: ['king khan', 'Dhakar Mastan']
-};
+const Hero = {
+    name: "SRK",
+    age: 60,
+    movies: ["Ra-One", "Paathan", "Jawan"]
+}
 
-// 1. template string
-const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
-console.log(about)
+// 1. Template String
+const aboutHero = `Name of my favorite hero is ${Hero.name}. His age is ${Hero.age}. His movies number is ${numbers[2]}. My favorite movie is ${Hero.movies[0]}.`;
 
-// 2. arrow function
+console.log(aboutHero);
+
+// 2. Arrow Function
 const getFiftyFive = () => 55;
+console.log(getFiftyFive());
+
 const addSixtyFive = num => num + 65;
+console.log(addSixtyFive(35));
+
 const isEven = x => x % 2 == 0;
+console.log(isEven(69));
+
 const addThree = (x, y, z) => x + y + z;
+console.log(addThree(10, 20, 30));
+
 const doMath = (num1, num2) => {
     const sum = num1 + num2;
     return sum;
 }
+console.log(doMath(6, 6));
 
-// spread operator
+
+// Spread Operator
 const newNumbers = [...numbers];
-// create a new array from an older array and add an element
+// Create a new array from an older array and add an element
 const currentNumbers = [...numbers, 55];
 
-
 numbers.push(99);
 numbers.push(99);
 numbers.push(99);
-
-
-
-
 
 console.log(numbers);
 console.log(newNumbers);
